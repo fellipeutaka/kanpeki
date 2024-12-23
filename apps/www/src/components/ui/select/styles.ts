c38a658eacba2dfdcc3220330433fea3013ca9b0
Fellipe Utaka <fellipeutaka@gmail.com>
2024-12-23 12:49:31 -0300

    feat(www): add select component

---
+++ apps/www/src/components/ui/select/styles.ts
@@ -0,0 +1,10 @@
+import { cva } from "~/lib/cva";
+
+export const SelectStyles = {
+  Trigger: cva({
+    base: [
+      "flex h-9 w-full items-center justify-between whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm outline-none",
+      "ring-offset-bg placeholder:text-muted-fg focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
+    ],
+  }),
+};
