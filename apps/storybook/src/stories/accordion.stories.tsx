5049114de442ff36febd874bf96a725313539f2f
Fellipe Utaka <fellipeutaka@gmail.com>
2025-02-18 14:58:52 -0300

    feat(storybook): add alert docs

--- apps/storybook/src/stories/accordion.stories.tsx
+++ apps/storybook/src/stories/accordion.stories.tsx
@@ -4,29 +4,7 @@ import { Accordion } from "~/components/ui/accordion";
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: "Components/Accordion",
-  component: (props) => (
-    <Accordion.Root className="w-96" {...props}>
-      <Accordion.Item>
-        <Accordion.Trigger>Is it accessible?</Accordion.Trigger>
-        <Accordion.Content>
-          Yes. It adheres to the WAI-ARIA design pattern.
-        </Accordion.Content>
-      </Accordion.Item>
-      <Accordion.Item>
-        <Accordion.Trigger>Is it styled?</Accordion.Trigger>
-        <Accordion.Content>
-          Yes. It comes with default styles that matches the other
-          components&apos; aesthetic.
-        </Accordion.Content>
-      </Accordion.Item>
-      <Accordion.Item>
-        <Accordion.Trigger>Is it animated?</Accordion.Trigger>
-        <Accordion.Content>
-          Yes. It's animated by default, but you can disable it if you prefer.
-        </Accordion.Content>
-      </Accordion.Item>
-    </Accordion.Root>
-  ),
+  component: Accordion.Root,
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
     isDisabled: { control: "boolean" },
@@ -35,6 +13,7 @@ const meta = {
   args: {
     isDisabled: false,
     allowsMultipleExpanded: false,
+    className: "w-96",
   },
 } satisfies Meta<typeof Accordion.Root>;
 
@@ -42,4 +21,28 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Default: Story = {};
+export const Default: Story = {
+  args: {
+    children: [
+      <Accordion.Item key="1">
+        <Accordion.Trigger>Is it accessible?</Accordion.Trigger>
+        <Accordion.Content>
+          Yes. It adheres to the WAI-ARIA design pattern.
+        </Accordion.Content>
+      </Accordion.Item>,
+      <Accordion.Item key="2">
+        <Accordion.Trigger>Is it styled?</Accordion.Trigger>
+        <Accordion.Content>
+          Yes. It comes with default styles that matches the other
+          components&apos; aesthetic.
+        </Accordion.Content>
+      </Accordion.Item>,
+      <Accordion.Item key="3">
+        <Accordion.Trigger>Is it animated?</Accordion.Trigger>
+        <Accordion.Content>
+          Yes. It's animated by default, but you can disable it if you prefer.
+        </Accordion.Content>
+      </Accordion.Item>,
+    ],
+  },
+};
