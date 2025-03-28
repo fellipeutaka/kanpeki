3381dc37e533e1f03821137ad8a130dee8f154e8
Fellipe Utaka <fellipeutaka@gmail.com>
2025-03-28 17:15:25 -0300

    perf(www): set edge runtime for search api route

--- apps/www/src/app/api/search/route.ts
+++ apps/www/src/app/api/search/route.ts
@@ -1,4 +1,6 @@
 import { createFromSource } from "fumadocs-core/search/server";
 import { source } from "~/lib/source";
 
+export const runtime = "edge";
+
 export const { GET } = createFromSource(source);
